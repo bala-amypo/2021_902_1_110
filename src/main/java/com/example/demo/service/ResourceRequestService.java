e26c89c2a5f5a27eba487a5a0f80d82e325a83ee
bala-amypo <bala@amypo.in>
2025-12-21 18:25:04 +0000

    User commit

--- src/main/java/com/example/demo/service/ResourceRequestService.java
+++ src/main/java/com/example/demo/service/ResourceRequestService.java
@@ -1,3 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.ResourceRequest;
+import java.util.List;
+
 public interface ResourceRequestService {
     ResourceRequest createRequest(Long userId, ResourceRequest request);
     ResourceRequest getRequest(Long id);
--- src/main/java/com/example/demo/service/ResourceService.java
+++ src/main/java/com/example/demo/service/ResourceService.java
@@ -1,3 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Resource;
+import java.util.List;
+
 public interface ResourceService {
     Resource createResource(Resource resource);
     Resource getResource(Long id);
