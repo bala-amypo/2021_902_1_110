1b68970f2f4731e933072f854da08dfa2147de50
bala-amypo <bala@amypo.in>
2025-12-21 16:13:00 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/ResourceAllocationController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.ResourceAllocation;
+import com.example.demo.service.ResourceAllocationService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/allocations")
+public class ResourceAllocationController {
+
+    private final ResourceAllocationService service;
+
+    public ResourceAllocationController(ResourceAllocationService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/auto/{requestId}")
+    public ResourceAllocation autoAllocate(@PathVariable Long requestId) {
+        return service.autoAllocate(requestId);
+    }
+
+    @GetMapping
+    public List<ResourceAllocation> getAll() {
+        return service.getAllAllocations();
+    }
+
+    @GetMapping("/{id}")
+    public ResourceAllocation get(@PathVariable Long id) {
+        return service.getAllocation(id);
+    }
+}
