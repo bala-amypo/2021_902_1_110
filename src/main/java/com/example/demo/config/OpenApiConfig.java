789bba2c5b2297f480561c1b8bf382253eb96f04
bala-amypo <bala@amypo.in>
2025-12-23 04:45:43 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -2,23 +2,25 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
-   @Bean
-public OpenAPI api() {
-    return new OpenAPI()
-            .info(new Info()
-                    .title("Resource Allocation Optimizer API")
-                    .version("1.0"))
-            .servers(List.of(
-                    new Server()
-                            .url("http://9036.408procr.amypo.ai")
-                            .description("Backend Server")
-            ));
-}
-
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Resource Allocation Optimizer API")
+                        .version("1.0"))
+                .servers(List.of(
+                        new Server()
+                                .url("http://9036.408procr.amypo.ai")
+                                .description("Backend Server")
+                ));
+    }
 }
