f2d06ed9147e83996d78feed20bdf876729c9878
bala-amypo <bala@amypo.in>
2025-12-26 09:55:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/AllocationRule.java
+++ src/main/java/com/example/demo/entity/AllocationRule.java
@@ -24,5 +24,41 @@ public class AllocationRule {
         this.createdAt = LocalDateTime.now();
     }
 
-    // getters and setters
+    // ===== GETTERS & SETTERS =====
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+
+    public String getRuleType() {
+        return ruleType;
+    }
+
+    public void setRuleType(String ruleType) {
+        this.ruleType = ruleType;
+    }
+
+    public Integer getPriorityWeight() {
+        return priorityWeight;
+    }
+
+    public void setPriorityWeight(Integer priorityWeight) {
+        this.priorityWeight = priorityWeight;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 }
