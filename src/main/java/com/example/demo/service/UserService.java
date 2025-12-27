25c88718371fbdea86c5ea08294bd1ded045e801
bala-amypo <bala@amypo.in>
2025-12-27 06:22:02 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -4,10 +4,10 @@ import com.example.demo.entity.User;
 
 public interface UserService {
 
-    // REQUIRED BY TESTS
+    
     User registerUser(User user);
 
-    // Used by controllers
+
     User saveUser(User user);
 
     User getUser(Long id);
